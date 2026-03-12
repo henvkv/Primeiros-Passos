62b62901f0db1a60e543a5d3d2bc4e45729c66b3
Henrique <r.dourado1404@gmail.com>
2026-03-11 21:38:35 -0300

    update

--- Dicionários/d91.py
+++ Dicionários/d91.py
@@ -6,7 +6,7 @@ valores = dict()
 for j in range(1, 5):
     valores[f'jogador{j}'] = random.randint(1, 6)
 
-valores_ordenado = dict(sorted(valores.items(), key=lambda item: item[1], reverse=True))
+ranking = sorted(valores.items(), key=lambda item: item[1], reverse=True)
 
 print('Valores sorteados:')
 for k, v in valores.items():
@@ -15,8 +15,5 @@ for k, v in valores.items():
 
 print('\nRanking dos jogadores:')
 
-for l in range(1,5):
-    print(f'{l}º Lugar:', end='')
-
-for k, v in valores_ordenado.items():
-    print(f'{k} com {v}')
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com o número {v[1]}')
